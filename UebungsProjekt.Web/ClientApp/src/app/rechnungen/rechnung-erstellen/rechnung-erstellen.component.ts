c5271fa31efd17221bc66a8c8bfbf3e96322dbfa
Christian Stark <christian.stark@qualitybytes.de>
2021-02-16 14:16:43 +0100

    Added loading spinner

--- UebungsProjekt.Web/ClientApp/src/app/rechnungen/rechnung-erstellen/rechnung-erstellen.component.ts
+++ UebungsProjekt.Web/ClientApp/src/app/rechnungen/rechnung-erstellen/rechnung-erstellen.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { CreateRechnungDto, RootObject } from 'src/app/api/rechnungen/create-rechnung-dto';
@@ -11,65 +12,73 @@ import { RechnungService } from '../rechnung.service';
   styleUrls: ['./rechnung-erstellen.component.css']
 })
 export class RechnungErstellenComponent implements OnInit {
+  @ViewChild('form', { static: false }) form: NgForm;
   config: reportingcloudConfiguration
-  @ViewChild('form', {static: false}) form: NgForm;
-  constructor(private rechnungService: RechnungService) { }
-
   rechnung: Rechnung;
   rechnungPDF;
-
   dto;
+  isLoading = false;
+
+  myDate: Date;
+  myUtcDate;
+
+
+
+  constructor(private rechnungService: RechnungService) { }
   ngOnInit() {
     this.config = new reportingcloudConfiguration();
   }
 
-
   createInvoice() {
-  this.dto = new RootObject();
-  this.dto.mergeData = Array<CreateRechnungDto>();
+    this.isLoading = true;
+    this.dto = new RootObject();
+    this.dto.mergeData = Array<CreateRechnungDto>();
 
-   this.rechnung = this.form.value;
-   this.dto.mergeData.push(new CreateRechnungDto(
-    this.rechnung.yourcompany_companyname ? this.rechnung.yourcompany_companyname : undefined,
-    this.rechnung.yourcompany_zip ? this.rechnung.yourcompany_zip : undefined,
-    this.rechnung.yourcompany_city ? this.rechnung.yourcompany_city : undefined,
-    this.rechnung.yourcompany_street ? this.rechnung.yourcompany_street : undefined,
-    this.rechnung.yourcompany_phone ? this.rechnung.yourcompany_phone : undefined,
-    this.rechnung.yourcompany_fax ? this.rechnung.yourcompany_fax : undefined,
-    this.rechnung.yourcompany_url ? this.rechnung.yourcompany_url : undefined,
-    this.rechnung.yourcompany_email ? this.rechnung.yourcompany_email : undefined,
-    this.rechnung.invoice_no ? this.rechnung.yourcompany_companyname : undefined,
-    this.rechnung.billto_name ? this.rechnung.billto_name : undefined,
-    this.rechnung.billto_companyname ? this.rechnung.billto_companyname : undefined,
-    this.rechnung.billto_customerid ? this.rechnung.billto_customerid : undefined,
-    this.rechnung.billto_zip ? this.rechnung.billto_zip : undefined,
-    this.rechnung.billto_city ? this.rechnung.billto_city : undefined,
-    this.rechnung.billto_street ? this.rechnung.billto_street : undefined,
-    this.rechnung.billto_phone ? this.rechnung.billto_phone : undefined,
+    this.rechnung = this.form.value;
+    this.dto.mergeData.push(new CreateRechnungDto(
+      this.rechnung.yourcompany_companyname ? this.rechnung.yourcompany_companyname : undefined,
+      this.rechnung.yourcompany_zip ? this.rechnung.yourcompany_zip : undefined,
+      this.rechnung.yourcompany_city ? this.rechnung.yourcompany_city : undefined,
+      this.rechnung.yourcompany_street ? this.rechnung.yourcompany_street : undefined,
+      this.rechnung.yourcompany_phone ? this.rechnung.yourcompany_phone : undefined,
+      this.rechnung.yourcompany_fax ? this.rechnung.yourcompany_fax : undefined,
+      this.rechnung.yourcompany_url ? this.rechnung.yourcompany_url : undefined,
+      this.rechnung.yourcompany_email ? this.rechnung.yourcompany_email : undefined,
+      this.rechnung.invoice_no ? this.rechnung.yourcompany_companyname : undefined,
+      this.rechnung.billto_name ? this.rechnung.billto_name : undefined,
+      this.rechnung.billto_companyname ? this.rechnung.billto_companyname : undefined,
+      this.rechnung.billto_customerid ? this.rechnung.billto_customerid : undefined,
+      this.rechnung.billto_zip ? this.rechnung.billto_zip : undefined,
+      this.rechnung.billto_city ? this.rechnung.billto_city : undefined,
+      this.rechnung.billto_street ? this.rechnung.billto_street : undefined,
+      this.rechnung.billto_phone ? this.rechnung.billto_phone : undefined,
 
-    //Noch nicht in Form eingefügt:
-    this.rechnung.payment_due ? this.rechnung.payment_due : undefined,
-    this.rechnung.payment_terms ? this.rechnung.payment_terms : undefined,
-    this.rechnung.salesperson_name ? this.rechnung.salesperson_name : undefined,
-    this.rechnung.delivery_date ? this.rechnung.delivery_date : undefined,
-    this.rechnung.delivery_method ? this.rechnung.delivery_method : undefined,
-    this.rechnung.delivery_method_terms ? this.rechnung.delivery_method_terms : undefined,
-    this.rechnung.recipient_name ? this.rechnung.recipient_name : undefined,
-    this.rechnung.recipient_companyname ? this.rechnung.recipient_companyname : undefined,
-    this.rechnung.recipient_zip ? this.rechnung.recipient_zip : undefined,
-    this.rechnung.recipient_city ? this.rechnung.recipient_city : undefined,
-    this.rechnung.recipient_street ? this.rechnung.recipient_street : undefined,
-    this.rechnung.recipient_phone ? this.rechnung.recipient_phone : undefined,
-    this.rechnung.item ? this.rechnung.item : undefined,
-    this.rechnung.total_discount ? this.rechnung.total_discount : undefined,
-    this.rechnung.total_sub ? this.rechnung.total_sub : undefined,
-    this.rechnung.total_tax ? this.rechnung.total_tax : undefined,
-    this.rechnung.total ? this.rechnung.total : undefined,
-     
-   ))
-   console.log(JSON.stringify(this.dto));
-   this.config.postedData = this.dto;
-   this.rechnungService.createInvoice(this.config.url, this.config.postedData, this.config.httpOptions).subscribe(r => console.log(this.rechnungPDF = r));
+      this.rechnung.payment_due ? this.rechnung.payment_due : undefined,
+      this.rechnung.payment_terms ? this.rechnung.payment_terms : undefined,
+      this.rechnung.salesperson_name ? this.rechnung.salesperson_name : undefined,
+      this.rechnung.delivery_date ? this.rechnung.delivery_date : undefined,
+      this.rechnung.delivery_method ? this.rechnung.delivery_method : undefined,
+      this.rechnung.delivery_method_terms ? this.rechnung.delivery_method_terms : undefined,
+
+
+      this.rechnung.recipient_name ? this.rechnung.recipient_name : undefined,
+      this.rechnung.recipient_companyname ? this.rechnung.recipient_companyname : undefined,
+      this.rechnung.recipient_zip ? this.rechnung.recipient_zip : undefined,
+      this.rechnung.recipient_city ? this.rechnung.recipient_city : undefined,
+      this.rechnung.recipient_street ? this.rechnung.recipient_street : undefined,
+      this.rechnung.recipient_phone ? this.rechnung.recipient_phone : undefined,
+      //Noch nicht in Form eingefügt:
+
+      this.rechnung.item ? this.rechnung.item : undefined,
+      this.rechnung.total_discount ? this.rechnung.total_discount : undefined,
+      this.rechnung.total_sub ? this.rechnung.total_sub : undefined,
+      this.rechnung.total_tax ? this.rechnung.total_tax : undefined,
+      this.rechnung.total ? this.rechnung.total : undefined,
+
+    ))
+    this.config.postedData = this.dto;
+    console.log(this.dto);
+    this.rechnungService.createInvoice(this.config.url, this.config.postedData, this.config.httpOptions).subscribe(r => {this.rechnungPDF = r; this.isLoading = false;});
   }
 
   setOwnInformations() {
@@ -100,22 +109,25 @@ export class RechnungErstellenComponent implements OnInit {
       billto_zip: "50667",
       billto_city: "Köln",
       billto_street: "Dingensstraße 94",
-      billto_phone: "030340340934"
+      billto_phone: "030340340934",
+
+      recipient_name: "Dirk Bauer",
+      recipient_companyname: "Höfer System Solutions",
+      recipient_zip: "50667",
+      recipient_city: "Köln",
+      recipient_street: "Dingensstraße 94",
+      recipient_phone: "030340340934"
     });
+
   }
 
-  
-  downloadPdf(base64String, fileName) {
+
+  downloadPdf() {
     const source = `data:application/pdf;base64,${this.rechnungPDF}`;
     const link = document.createElement("a");
     link.href = source;
-    link.download = `${fileName}.pdf`
+    link.download = "Rechnung" + formatDate(new Date(), 'dd/MM/yyyy HH:mm:ss', 'en', '+0200');
     link.click();
-  }
-
-  onClickDownloadPdf(){
-    let base64String = this.rechnungPDF;
-    this.downloadPdf(base64String,"sample");
   }
 
 }
